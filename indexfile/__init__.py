24de93707893f31db3c2d3da8a02aa8666b02128
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-07-09 16:46:23 +0200

    Hacked module to use getattr

--- indexfile/__init__.py
+++ indexfile/__init__.py
@@ -1,3 +1,20 @@
+import sys
+
 """ The indexfile API """
 
-__version__ = "0.9-alpha"
+class IndexFileWrapper(object):
+    __version__ = "0.9-alpha"
+
+    path_key = 'path'
+    id_key = 'labExpId'
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __getattr__(self, name):
+        if name == 'file_info':
+            return [self.path_key, 'md5', 'size']
+        else:
+            raise AttributeError("%r object has no attribute %r" % (type(self.wrapped).__name__, name))
+
+sys.modules[__name__] = IndexFileWrapper(sys.modules[__name__])
