507af3f2b7a84698f918c1d90232a8aa61bcf1da
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-07-11 15:52:43 +0200

    Added first tests

--- test_index.py
+++ test_index.py
@@ -2,7 +2,13 @@ import os
 import pytest
 from indexfile.index import *
 
+def test_create_empty_dataset():
+    dataset = Dataset()
+
 def test_create_dataset():
-    info = {'labExpId': '1', 'sex':'M', 'age':'65'}
-    dataset = Dataset(info)
-    print str(dataset)
+    info = {'labExpId': '1', 'sex':'M', 'age':'65', 'readType':'2x76'}
+    dataset = Dataset(**info)
+    assert dataset.id == '1'
+    assert dataset.sex == 'M'
+    assert dataset.age == '65'
+    assert dataset.single_end == False
