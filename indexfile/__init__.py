b3da88a4e833337acce5b939873167621918f164
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-08-09 17:16:39 +0200

    Cleanup __init__.py

--- indexfile/__init__.py
+++ indexfile/__init__.py
@@ -1,10 +1,3 @@
 """ The IndexFile Package """
 
 __version__ = "0.9-alpha"
-
-_format = {'id':'labExpId','path':'path'}
-
-_file_info = ['view', 'md5', 'size']
-_meta_info = []
-
-_out_format = {'kw_sep':' ','sep':'=','trail':';'}
