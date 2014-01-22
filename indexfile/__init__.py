09fe7c58b3b8f54479c183ad74f732573f289fd6
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-01-22 12:58:24 +0100

    Use logging module. Resolves #6

--- indexfile/__init__.py
+++ indexfile/__init__.py
@@ -2,5 +2,28 @@
 
 """
 
+import logging
+
 __name__ = "idxtools"
 __version__ = "0.9b2"
+_log_level = 30
+
+
+def getLogger(name):
+    logger = logging.getLogger(name)
+    logger.handler = []
+    return logger
+
+def setLogLevel(level):
+    log.setLevel(getattr(logging, str(level).upper(), 30))
+
+log = logging.getLogger()
+log.handler = []
+log.propagate = False
+setLogLevel(_log_level)
+
+ch = logging.StreamHandler()
+fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(fmt)
+log.addHandler(ch)
+
