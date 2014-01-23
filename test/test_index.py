9741d88e022292e46f050a2a48175ffeb92b7b15
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-01-23 14:28:31 +0100

    Add test for replicates

--- test/test_index.py
+++ test/test_index.py
@@ -11,3 +11,20 @@ def test_create_dataset():
     assert dataset.id == '1'
     assert dataset.sex == 'M'
     assert dataset.age == '65'
+
+def test_replicates():
+    i = Index('test/data/index.txt')
+    i.set_format('test/data/format.json')
+    i.open()
+    reps = i.find_replicates(id="EWP.1,EWP.2")
+    d = reps[0]
+    others = reps[1:]
+    m = d.merge(others)
+    for k in m._metadata.keys():
+        vals = [d._metadata[k] for d in reps]
+        if len(set(vals)) > 1:
+            if k == 'id':
+                vals = ','.join(vals)
+            assert m._metadata[k] == vals
+        else:
+            assert m._metadata[k] == vals[0]
