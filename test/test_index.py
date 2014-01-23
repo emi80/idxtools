a937e22f461924d72f04f7d5c86a63147ae6e6e3
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-01-23 15:01:36 +0100

    Add test for list values in metadata

--- test/test_index.py
+++ test/test_index.py
@@ -28,3 +28,11 @@ def test_replicates():
             assert m._metadata[k] == vals
         else:
             assert m._metadata[k] == vals[0]
+
+def test_replicates_w_metadata():
+    i = Index('test/data/index.txt')
+    i.set_format('test/data/format.json')
+    i.open()
+    i.insert(id='aWL3.1,aWL3.2', path='test/data/format.json', type='json', view='json')
+    i.select(id='aWL3.1,aWL3.2')
+    i.remove(path='test/data/format.json', clear=True)
