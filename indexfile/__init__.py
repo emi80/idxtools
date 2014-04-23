ee154dcabf6cc30b51f00d208582c063a6c00d0d
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-04-23 12:39:54 +0200

    Move default format dictionary and add docstrings

--- indexfile/__init__.py
+++ indexfile/__init__.py
@@ -8,13 +8,32 @@ __name__ = "idxtools"
 __version__ = "0.9.2.dev1"
 _log_level = 30
 
+# default format
+
+default_format = {
+    "colsep": "\t",
+    "fileinfo": [
+        "path",
+        "size",
+        "md5",
+        "type",
+        "view"
+    ],
+    "kw_sep": " ",
+    "sep": "=",
+    "trail": ";"
+}
+
 
 def getLogger(name):
+    """Return the logger given the name"""
     logger = logging.getLogger(name)
     logger.handler = []
     return logger
 
+
 def setLogLevel(level):
+    """Set logger loglevel"""
     log.setLevel(getattr(logging, str(level).upper(), 30))
 
 log = logging.getLogger()
@@ -26,4 +45,3 @@ ch = logging.StreamHandler()
 fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s', '%m/%d/%Y %H:%M:%S')
 ch.setFormatter(fmt)
 log.addHandler(ch)
-
