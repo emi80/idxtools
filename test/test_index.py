e64a011357845c485431531808f2d0e43503eb49
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-04-23 12:42:19 +0200

    Add unit tests for the Dataset class

--- test/test_index.py
+++ test/test_index.py
@@ -2,16 +2,6 @@ import os
 import pytest
 from indexfile.index import *
 
-def test_create_empty_dataset():
-    dataset = Dataset()
-
-def test_create_dataset():
-    info = {'id': '1', 'sex':'M', 'age':'65'}
-    dataset = Dataset(**info)
-    assert dataset.id == '1'
-    assert dataset.sex == 'M'
-    assert dataset.age == '65'
-
 def test_replicates():
     i = Index('test/data/index.txt')
     i.set_format('test/data/format.json')
