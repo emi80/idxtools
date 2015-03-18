01a7e3fc821d164ee3bce2543545289bec440b2c
Emilio Palumbo <emiliopalumbo@gmail.com>
2015-03-18 16:02:41 +0100

    Fix variables in changelog script

--- changelog.sh
+++ changelog.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 start=$1
 end=$2
-git log $1...$2 --pretty=format:'- %s ([view commit](http://github.com/emi80/idxtools/commit/%H))' --reverse | grep "#"
+git log $start...$end --pretty=format:'- %s ([view commit](http://github.com/emi80/idxtools/commit/%H))' --reverse | grep "#"
