440e560b3893422724c0af709cb38e7d27e14741
Emilio Palumbo <emiliopalumbo@gmail.com>
2015-09-08 16:58:54 +0200

    config - remove set_index_format and create Config class extending DotDict

--- indexfile/config.py
+++ indexfile/config.py
@@ -32,25 +32,40 @@ OUTPUT_FORMATS = {
     'yaml': {}
 }
 
-# utility functions
-def set_index_format(input_format, reset=False):
-	"""Update format"""
-	format_dict = input_format
-	if isinstance(input_format, str):
-		input_format = open(input_format, 'r')
-	if isinstance(input_format, file):
-		format_dict = yaml.load(input_format)
-	if not hasattr(config, 'format') or reset:
-		config.format = DotDict(format_dict)
-	else:
-		config.format.update(format_dict)
 
 def set_config_defaults():
     """Set default values for config"""
     config.hash_algorithm = 'md5'
     config.map_only = False
-    set_index_format(DEFAULT_FORMAT, True)
+    config.loglevel = indexfile._log_level
+    config.format = DEFAULT_FORMAT
+
+class Config(DotDict):
+    """Config class that extends DotDict"""
+
+    def update(self, other=None, **kwargs):
+        """Update a DotDict supporting nested attribute access"""
+        if 'format' in kwargs:
+            kwargs['format'] = Config._get_dict(kwargs['format'])
+        # call update from superclass
+        super(Config, self).update(other, **kwargs)
+
+    def __setitem__(self, key, value):
+        if key == 'format':
+           value = Config._get_dict(value) 
+        DotDict.__setitem__(self, key, value)
+
+    @staticmethod
+    def _get_dict(fp):
+        if isinstance(fp, str):
+            fp = open(fp, 'r')
+        if isinstance(fp, file):
+            fp = yaml.load(fp)
+        return fp
+
+    __setattr__ = __setitem__
+
 
 # initialize default config
-config = DotDict()
+config = Config()
 set_config_defaults()
