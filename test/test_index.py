f1b2416fdb60f95d8a06d4b9e72ec50f6a510320
Emilio Palumbo <emiliopalumbo@gmail.com>
2015-09-23 16:41:56 +0200

    Update index tests

--- test/test_index.py
+++ test/test_index.py
@@ -1,9 +1,14 @@
 """Unit test for the Index class"""
 import sys
+import types
 import pytest
 import StringIO
 from indexfile.index import Index, parse_line, to_str
-from indexfile.config import config, set_config_defaults
+from indexfile.config import config
+@pytest.fixture(scope='session', autouse=True)
+def setup_config():
+    config.fileinfo.append('view')
+
 
 def test_create_empty():
     """Create empty index"""
@@ -14,7 +19,7 @@ def test_create_empty():
 
 def test_create_path():
     """Create index from file path"""
-    config.update(format="test/data/format.json")
+    config.update("test/data/config.json")
     i = Index("test/data/index.txt")
     assert i is not None
     assert len(i) == 36
@@ -49,7 +54,7 @@ def test_create_stdin():
 
 def test_create_defer_format():
     """Create index from file handle"""
-    config.update(format="test/data/format.json")
+    config.update("test/data/config.json")
     i = Index("test/data/index.txt")
     assert i is not None
     i.load()
@@ -58,19 +63,21 @@ def test_create_defer_format():
 
 def test_create_empty_w_format():
     """Create empty index with custom format"""
-    form = {
-        "id": "labExpId",
-        "colsep": "\t",
+    conf = {
+        "id_desc": "labExpId",
         "fileinfo": [
             "path",
             "size",
             "md5"
         ],
-        "kw_sep": " ",
-        "sep": "=",
-        "trail": ";"
+        "format": {
+            "col_sep": "\t",
+            "tag_sep": " ",
+            "kw_sep": "=",
+            "kw_trail": ";"
+        }
     }
-    config.format.update(form)
+    config.update(conf)
     i = Index()
     assert i is not None
     assert len(i) == 0
@@ -105,7 +112,7 @@ def test_load_file():
 
 def test_tsv_with_files():
     """Test open with file parameter"""
-    config.update(format="test/data/format.json")
+    config.update("test/data/config.json")
     i = Index('test/data/table_with_files.tsv')
     assert i is not None
     assert len(i) == 20
@@ -134,7 +141,7 @@ def test_checksum_empty():
 
 def test_insert():
     """Test insertion into the index"""
-    set_config_defaults()
+    config.reset()
     i = Index()
     i.insert(id='1', age=65, path='test.txt', type='txt')
     assert len(i.datasets) == 1
@@ -155,7 +162,8 @@ def test_insert():
 def test_insert_non_default_fileinfo():
     """Test insertion into the index with specified fileinfo"""
     i = Index()
-    config.format['fileinfo'] = ['path', 'type', 'date']
+    old_fileinfo = config.fileinfo
+    config.fileinfo = {'path', 'type', 'date'}
     i.insert(id='1', age=65, path='test.txt', type='txt', date='20150304')
     assert len(i.datasets) == 1
     dataset = i.datasets.get('1')
@@ -172,6 +180,7 @@ def test_insert_non_default_fileinfo():
     assert hasattr(dataset['test.txt'], 'date')
     assert dataset['test.txt'].type == 'txt'
     assert dataset['test.txt'].date == '20150304'
+    config.fileinfo = old_fileinfo
 
 
 def test_insert_update():
@@ -220,15 +229,16 @@ def test_insert_force_update():
 
 def test_add_file_external():
     index = '''.\tage=-; cell=Neutrophils; dataType=RNA-Seq; dateSubmittedFirst=2012-10-17T09:49:23+0200; donorId=C000XW; ethnicity=NA; lab=MPIMG; labExpId=ERR180946; labProtocolId=C000XWB1; libProtocol=I_bc_pelib_858; localization="Primary Cell"; quality=phred; readStrand=MATE2_SENSE; readType=2x76D; rnaExtract=total; seqPlatform=ILLUMINA; seqRun=1; sex=Male; sraSampleAccession=ERS150362; sraStudyAccession=ERP001664; tissue="Cord blood";'''
-    config.update(format="test/data/tsv_format.json")
+    config.update("test/data/tsv_config.json")
+    config.fileinfo.append('view')
     i = Index()
-    i.insert(**parse_line(index, **config.format))
+    i.insert(**parse_line(index))
     i.insert(id="ERR180946", path="/users/rg/epalumbo/projects/BluePrint/reads/20130805/data/ERR180946_1.fastq.gz", view="FastqRd1",type="fastq")
-    assert i.export(map=None)[0] == '''/users/rg/epalumbo/projects/BluePrint/reads/20130805/data/ERR180946_1.fastq.gz\tage=-; cell=Neutrophils; dataType=RNA-Seq; dateSubmittedFirst=2012-10-17T09:49:23+0200; donorId=C000XW; ethnicity=NA; lab=MPIMG; labExpId=ERR180946; labProtocolId=C000XWB1; libProtocol=I_bc_pelib_858; localization="Primary Cell"; quality=phred; readStrand=MATE2_SENSE; readType=2x76D; rnaExtract=total; seqPlatform=ILLUMINA; seqRun=1; sex=Male; sraSampleAccession=ERS150362; sraStudyAccession=ERP001664; tissue="Cord blood"; type=fastq; view=FastqRd1;'''
+    assert i.export().next() == '''/users/rg/epalumbo/projects/BluePrint/reads/20130805/data/ERR180946_1.fastq.gz\tage=-; cell=Neutrophils; dataType=RNA-Seq; dateSubmittedFirst=2012-10-17T09:49:23+0200; donorId=C000XW; ethnicity=NA; lab=MPIMG; labExpId=ERR180946; labProtocolId=C000XWB1; libProtocol=I_bc_pelib_858; localization="Primary Cell"; quality=phred; readStrand=MATE2_SENSE; readType=2x76D; rnaExtract=total; seqPlatform=ILLUMINA; seqRun=1; sex=Male; sraSampleAccession=ERS150362; sraStudyAccession=ERP001664; tissue="Cord blood"; type=fastq; view=FastqRd1;'''
 
 
 def test_lookup_simple_dataset():
-    set_config_defaults()
+    config.reset()
     i = Index()
     i.insert(id='1', age=65, path='test.txt', type='txt')
     selected = i.lookup(id='1')
@@ -310,7 +320,7 @@ def test_lookup_multiple_or():
 
 
 def test_lookup_no_path():
-    config.update(format="test/data/format.json")
+    config.update("test/data/config.json")
     i = Index('test/data/index.txt')
     assert i.datasets.get('WLP.2') is not None
     selected = i.lookup(id='WLP.2')
@@ -323,17 +333,17 @@ def test_lookup_no_path():
 
 def test_lookup_more_types_index_id():
     """Test export"""
-    config.update(format="test/data/format.json")
+    config.update("test/data/config.json")
     i = Index('test/data/index_gfs.txt')
     result = i.lookup(id='WWP.1')
-    assert result.export()[0][0] != '.'
+    assert result.export().next()[0] != '.'
 
 
 def test_lookup_one_type_index():
     """Test export"""
     i = Index('test/data/index_gtfs.txt')
     result = i.lookup(type='gtf')
-    assert result.export()[0][0] != '.'
+    assert result.export().next()[0] != '.'
 
 
 def test_lookup_more_types_index():
@@ -342,21 +352,23 @@ def test_lookup_more_types_index():
     result = i.lookup(type='gtf')
     assert result != None
     assert result.datasets != {}
-    assert result.export()[0][0] != '.'
+    assert result.export().next()[0] != '.'
 
 
 def test_lookup_full_index():
     """Test export"""
     i = Index('test/data/index.txt')
     result = i.lookup(type='gtf')
-    assert result.export()[0][0] != '.'
+    assert result.export().next()[0] != '.'
 
 
 def test_remove_dataset():
+    config.reset()
     i = Index()
     i.insert(id='1', age=65, path='test.txt', type='txt')
     i.remove(id='1')
     assert len(i.datasets) == 0
+    setup_config()
 
 
 def test_remove_file():
@@ -385,140 +397,151 @@ def test_remove_fileinfo():
     assert len(i.datasets.get('1')) == 2
 
 
-def test_export():
+def test_export_w_map():
+    """Test export"""
+    config.update('test/data/config.json')
+    i = Index('test/data/index.txt')
+    exp = i.export(map_keys=True)
+    assert isinstance(exp, types.GeneratorType)
+    assert 'LIBRARY_ID' in exp.next()
+    assert len(list(exp)) == 215
+
+
+def test_export_w_map_one_tag():
     """Test export"""
     i = Index('test/data/index.txt')
-    exp = i.export()
-    assert type(exp) == list
-    assert len(exp) == 216
-    assert 'LIBRARY_ID' in exp[0]
+    exp = i.export(map_keys=True, tags='id')
+    assert isinstance(exp, types.GeneratorType)
+    assert 'LIBRARY_ID' in exp.next()
+    assert len(list(exp)) == 215
 
 
-def test_export_index_one_tag():
-    """Test export"""
-    i = Index('test/data/index.txt')
-    exp = i.export(tags='id')
-    assert type(exp) == list
-    assert len(exp) == 216
-    assert 'LIBRARY_ID' in exp[0]
-
-
-def test_export_oneline():
+def test_export_w_map_oneline():
     """Test export one known line"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export()
-    assert exp[0] == '''aWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\tLIBRARY_ID=aWL3.2; RNA_quantity=100; barcode=AR025; cell=anterior; dataType=rnaSeq; developmental_point=L3; library_Bioanalyser="5.8 ng/uL (30 nM) 08/04/2013"; localization=cell; max_peak=297; n_sequences=37478754; organism=dmel; pool_ID=2; readStrand=MATE1_SENSE; readType=2x75D; replicate=2; rnaExtract=longPolyA; sequence=ACTGAT(A); tissue=wing; type=gtf; view=TranscriptFB554;'''
+    exp = i.export(map_keys=True)
+    assert exp.next() == '''aWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\tLIBRARY_ID=aWL3.2; RNA_quantity=100; barcode=AR025; cell=anterior; dataType=rnaSeq; developmental_point=L3; library_Bioanalyser="5.8 ng/uL (30 nM) 08/04/2013"; localization=cell; max_peak=297; n_sequences=37478754; organism=dmel; pool_ID=2; readStrand=MATE1_SENSE; readType=2x75D; replicate=2; rnaExtract=longPolyA; sequence=ACTGAT(A); tissue=wing; type=gtf; view=TranscriptFB554;'''
 
 
 def test_export_no_map():
     """Test export"""
     i = Index('test/data/index.txt')
-    exp = i.export(map=None)
-    assert len(exp) == 216
-    assert 'labExpId' in exp[0]
-    assert 'fileinfo' not in exp[0]
+    exp = i.export()
+    first = exp.next()
+    assert 'labExpId' in first
+    assert 'fileinfo' not in first
+    assert len(list(exp)) == 215
 
 
 def test_export_no_format_no_map():
     """Test export"""
+    config.reset()
     i = Index()
     i.insert(id="myId", path="test/data/index.txt", type="text", view="TxtFile")
-    exp = i.export(map=None, output_format='tsv', tags=['id'])
-    assert len(exp) == 1
+    exp = list(i.export(output_format='tsv', tags=['id']))
     assert exp[0] == 'myId'
+    assert len(exp) == 1
+    setup_config()
 
 
 def test_export_no_map_tab_tags_no_miss():
     """Test export without missing values"""
+    config.update('test/data/config.json')
     i = Index('test/data/index.txt')
-    exp = i.export(map=None, output_format='tsv', tags=['id', 'path'],
+    exp = i.export(output_format='tsv', tags=['id', 'path'],
                    hide_missing=True)
-    assert len(exp) == 200
-    assert exp[0] == 'EL3.1\t/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1_5355_ATCACG.minusRaw.bigwig'
+    assert exp.next() == 'EL3.1\t/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1_5355_ATCACG.minusRaw.bigwig'
+    assert len(list(exp)) == 199
 
 
 def test_export_no_map_tab_repeated_tags():
     """Test export tab output with repeated tags"""
     i = Index('test/data/index.txt')
-    exp = i.export(map=None, output_format='tsv', tags=['id', 'id', 'path'],
+    exp = i.export(output_format='tsv', tags=['id', 'id', 'path'],
                    hide_missing=True)
-    assert len(exp) == 200
-    assert exp[0] == 'EL3.1\tEL3.1\t/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1_5355_ATCACG.minusRaw.bigwig'
+    assert exp.next() == 'EL3.1\tEL3.1\t/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1_5355_ATCACG.minusRaw.bigwig'
+    assert len(list(exp)) == 199
 
 
 def test_export_no_map_tab_path_template():
     """Test export tab output with path template"""
     i = Index('test/data/index.txt')
-    exp = i.export(map=None, output_format='tsv', tags=['path','{dirname}/{id}.{view}.{ext}'],
+    exp = i.export(output_format='tsv', tags=['path','{dirname}/{id}.{view}.{ext}'],
                    hide_missing=True)
-    assert len(exp) == 200
-    assert exp[0] == '/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1_5355_ATCACG.minusRaw.bigwig\t/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1.MinusRawSignal.bigwig'
+    assert exp.next() == '/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1_5355_ATCACG.minusRaw.bigwig\t/users/rg/epalumbo/projects/ERC/fly/bp.pipeline/EL3.1/EL3.1.MinusRawSignal.bigwig'
+    assert len(list(exp)) == 199
 
 
 def test_export_oneline_no_map():
     """Test export one known line"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export(map=None)
-    assert exp[0] == '''aWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\tadaptor=ACTGAT(A); age=L3; barcode=AR025; cell=anterior; dataType=rnaSeq; labExpId=aWL3.2; libBio="5.8 ng/uL (30 nM) 08/04/2013"; localization=cell; maxPeak=297; nReads=37478754; organism=dmel; poolId=2; readStrand=MATE1_SENSE; readType=2x75D; replicate=2; rnaExtract=longPolyA; rnaQuantity=100; tissue=wing; type=gtf; view=TranscriptFB554;'''
+    exp = i.export()
+    assert exp.next() == '''aWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\tadaptor=ACTGAT(A); age=L3; barcode=AR025; cell=anterior; dataType=rnaSeq; labExpId=aWL3.2; libBio="5.8 ng/uL (30 nM) 08/04/2013"; localization=cell; maxPeak=297; nReads=37478754; organism=dmel; poolId=2; readStrand=MATE1_SENSE; readType=2x75D; replicate=2; rnaExtract=longPolyA; rnaQuantity=100; tissue=wing; type=gtf; view=TranscriptFB554;'''
 
 
 def test_export_ol_no_map_tab_tags():
     """Test export"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export(map=None, output_format='tsv', tags=['id', 'path'])
-    print exp[0]
-    assert exp[0] == 'aWL3.2\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
+    exp = i.export(output_format='tsv', tags=['id', 'path'])
+    assert exp.next() == 'aWL3.2\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
 
 
 def test_export_ol_no_map_tab_path():
     """Test export"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export(map=None, output_format='tsv', tags=['path'])
-    assert exp[0] == 'aWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
+    exp = i.export(output_format='tsv', tags=['path'])
+    assert exp.next() == 'aWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
 
 
 def test_export_ol_no_map_tab_tags_header():
     """Test export"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export(map=None, output_format='tsv', tags=['id', 'path'],
+    exp = i.export(output_format='tsv', tags=['id', 'path'],
                    header=True)
-    assert exp[0] == 'labExpId\tpath'
-    assert exp[1] == 'aWL3.2\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
+    assert exp.next() == 'labExpId\tpath'
+    assert exp.next() == 'aWL3.2\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
 
 
 def test_export_ol_no_map_tab_repeated_tags_header():
     """Test export"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export(map=None, output_format='tsv', tags=['id', 'id', 'path'],
+    exp = i.export(output_format='tsv', tags=['id', 'id', 'path'],
                    header=True)
-    assert exp[0] == 'labExpId\tlabExpId\tpath'
-    assert exp[1] == 'aWL3.2\taWL3.2\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
+    assert exp.next() == 'labExpId\tlabExpId\tpath'
+    assert exp.next() == 'aWL3.2\taWL3.2\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf'
 
 
-def test_export_ol_no_map_tab_all():
+def test_export_ol_no_map_tsv_all():
     """Test export"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export(map=None, output_format='tsv')
-    assert exp[0] == '''ACTGAT(A)\tL3\tAR025\tanterior\trnaSeq\taWL3.2\t"5.8 ng/uL (30 nM) 08/04/2013"\tcell\t297\t37478754\tdmel\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\t2\tMATE1_SENSE\t2x75D\t2\tlongPolyA\t100\twing\tgtf\tTranscriptFB554'''
+    exp = i.export(output_format='tsv')
+    assert exp.next() == '''ACTGAT(A)\tL3\tAR025\tanterior\trnaSeq\taWL3.2\t"5.8 ng/uL (30 nM) 08/04/2013"\tcell\t297\t37478754\tdmel\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\t2\tMATE1_SENSE\t2x75D\t2\tlongPolyA\t100\twing\tgtf\tTranscriptFB554'''
+
+
+def test_export_ol_no_map_csv_all():
+    """Test export"""
+    i = Index('test/data/index_oneline.txt')
+    exp = i.export(output_format='csv')
+    assert exp.next() == '''ACTGAT(A),L3,AR025,anterior,rnaSeq,aWL3.2,"5.8 ng/uL (30 nM) 08/04/2013",cell,297,37478754,dmel,aWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf,2,MATE1_SENSE,2x75D,2,longPolyA,100,wing,gtf,TranscriptFB554'''
 
 
 def test_export_ol_no_map_tab_all_header():
     """Test export"""
     i = Index('test/data/index_oneline.txt')
-    exp = i.export(map=None, output_format='tsv', header=True)
-    assert exp[0] == 'adaptor\tage\tbarcode\tcell\tdataType\tlabExpId\tlibBio\tlocalization\tmaxPeak\tnReads\torganism\tpath\tpoolId\treadStrand\treadType\treplicate\trnaExtract\trnaQuantity\ttissue\ttype\tview'
-    assert exp[1] == '''ACTGAT(A)\tL3\tAR025\tanterior\trnaSeq\taWL3.2\t"5.8 ng/uL (30 nM) 08/04/2013"\tcell\t297\t37478754\tdmel\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\t2\tMATE1_SENSE\t2x75D\t2\tlongPolyA\t100\twing\tgtf\tTranscriptFB554'''
+    exp = i.export(output_format='tsv', header=True)
+    assert exp.next() == 'adaptor\tage\tbarcode\tcell\tdataType\tlabExpId\tlibBio\tlocalization\tmaxPeak\tnReads\torganism\tpath\tpoolId\treadStrand\treadType\treplicate\trnaExtract\trnaQuantity\ttissue\ttype\tview'
+    assert exp.next() == '''ACTGAT(A)\tL3\tAR025\tanterior\trnaSeq\taWL3.2\t"5.8 ng/uL (30 nM) 08/04/2013"\tcell\t297\t37478754\tdmel\taWL3.2/aWL3.2_4204_ACTGAT_transcript.gtf\t2\tMATE1_SENSE\t2x75D\t2\tlongPolyA\t100\twing\tgtf\tTranscriptFB554'''
 
 
 def test_replicates():
     """Test merged datasets"""
-    config.update(format="test/data/format.json")
+    config.update("test/data/config.json")
     i = Index('test/data/index.txt')
-    dsid = config.format.get('id','id')
-    reps = i.find_replicates(id="EWP.1,EWP.2")
+    dsid = config.id_desc
+    reps = list(i.find_replicates(id="EWP.1,EWP.2"))
     dataset = reps[0]
     others = reps[1:]
-    merged = dataset.merge(others, dsid=dsid)
+    merged = dataset.merge(others)
     for key in merged.keys():
         vals = [getattr(d, key) for d in reps]
         if len(set(vals)) > 1:
@@ -533,11 +556,11 @@ def test_replicates_w_metadata():
     """Test merged datasets with metadata"""
     i = Index('test/data/index.txt')
     i.insert(id='aWL3.1,aWL3.2',
-             path='test/data/format.json',
+             path='test/data/config.json',
              type='json',
              view='json')
     i.lookup(id='aWL3.1,aWL3.2')
-    i.remove(path='test/data/format.json', clear=True)
+    i.remove(path='test/data/config.json', clear=True)
 
 
 def test_format():
