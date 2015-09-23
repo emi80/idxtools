a67d50c0ce6aeba3fbabcfb523b1367f14c5ed5d
Emilio Palumbo <emiliopalumbo@gmail.com>
2015-09-23 16:32:29 +0200

    Update config tests

--- test/test_config.py
+++ test/test_config.py
@@ -1,43 +1,60 @@
+import os
 import yaml
-from indexfile.config import config, set_index_format, set_config_defaults
+import pytest
+from indexfile.config import config
 
-with open('test/data/format.json') as js:
-	test_format = yaml.load(js)
+env = os.environ
 
-
-def test_set_format_dict():
-	"""Test set format with JSON string"""
-	new_format = {
-	    "id": "labExpId",
-	    "colsep": "\\t",
-	    "fileinfo": [
-	        "path",
-	        "size",
-	        "md5"
-	    ],
-	    "kw_sep": " ",
-	    "sep": "=",
-	    "trail": ";"
-	}
-	set_index_format(new_format)
-	assert config.format
-	for k in new_format:
-		assert config.format[k] == new_format[k]
+with open('test/data/config.json') as js:
+  test_config = yaml.load(js)
 
 
-def test_set_format_path():
+def test_update_config_dict():
+  """Test set format with JSON string"""
+  new_config = {
+      "id_desc": "labExpId",
+      "fileinfo": [
+          "path",
+          "size",
+          "md5"
+      ],
+      "format": {
+        "col_sep": "\t",
+        "kw_sep": " ",
+        "tag_sep": "=",
+        "kw_trail": ";"
+      }
+  }
+  config.update(new_config)
+  assert config
+  for k in new_config:
+    if k == 'format':
+      for j in new_config[k]:
+        assert config[k][j] == new_config[k][j]
+    else:
+      assert config[k] == new_config[k]
+
+
+def test_update_config_path():
   """Test set format with file path"""
-  set_index_format('test/data/format.json')
-  assert config.format
-  for k in test_format:
-  	assert config.format[k] == test_format[k]
+  config.update('test/data/config.json')
+  assert config
+  for k in test_config:
+    if k == 'format':
+      for j in test_config[k]:
+        assert config[k][j] == test_config[k][j]
+    else:
+      assert config[k] == test_config[k]
 
 
-def test_set_format_file():
+def test_update_config_file():
   """Test set format with file path"""
-  fh = open('test/data/format.json', 'r')
-  set_index_format('test/data/format.json')
-  assert config.format
-  for k in test_format:
-  	assert config.format[k] == test_format[k]
-    
+  fh = open('test/data/config.json', 'r')
+  config.update(fh)
+  assert config
+  for k in test_config:
+    if k == 'format':
+      for j in test_config[k]:
+        assert config[k][j] == test_config[k][j]
+    else:
+      assert config[k] == test_config[k]
