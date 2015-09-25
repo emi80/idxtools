e69333cf049016c454460115c50e6ffe71b4baa4
Emilio Palumbo <emiliopalumbo@gmail.com>
2015-09-25 12:33:30 +0200

    CLI main - use file type for format to be able to check for file existence

--- indexfile/cli/indexfile_main.py
+++ indexfile/cli/indexfile_main.py
@@ -5,7 +5,7 @@ from indexfile.cli import *
 
 @click.command(cls=IndexfileCLI, context_settings=CONTEXT_SETTINGS)
 @click.option('-i', '--index', envvar=DEFAULT_ENV_INDEX, type=click.File('r'), default='-', help='The input index file.')
-@click.option('-f', '--format', envvar=DEFAULT_ENV_FORMAT, type=click.Path(), default=None, help='Index format specifications in JSON or YAML format.', show_default=True)
+@click.option('-f', '--format', envvar=DEFAULT_ENV_FORMAT, type=click.File('r'), default=None, help='Index format specifications in JSON or YAML format.', show_default=True)
 @click.option('--loglevel', type=click.Choice(log_levels()), default=config.loglevel, help='Set the log level to the desired level.', show_default=True)
 @click.version_option(indexfile.__version__)
 @click.pass_context
